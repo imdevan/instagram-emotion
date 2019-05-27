daa399adc503e167daece95c5077b66b003d3890
Devan Huapaya <huapayadevan@gmail.com>
2019-05-27 14:50:20 -0700

    Unpacked build folder

---
+++ precache-manifest.2dc821029171a3c1b948f080039baea6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f4ba6658f1ea2d3e467ded293c2cede",
+    "url": "/index.html"
+  },
+  {
+    "revision": "222d7490c9c5f00a85b7",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6d475bef879c688a97de",
+    "url": "/static/js/2.0dfe4c11.chunk.js"
+  },
+  {
+    "revision": "222d7490c9c5f00a85b7",
+    "url": "/static/js/main.e58b4721.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
